91a02b234806d69afae00154ccd261f6389dd691
Lucas Williams <wililupy@comcast.net>
2022-11-25 22:05:56 -0800

    Update clean_snaps.sh
    Added prompts

--- clean_snaps.sh
+++ clean_snaps.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Remove old revisions of snaps
 # Make sure that snaps (like snap-store) are disabled
-set -eu
 
-LANG=C snap list --all | awk '/disabled/{print $1, $3}' |
-    while read snapname revision; do
+snap list --all | awk '/disabled/{print $1}' |
+    while read snapname; do
+        echo "Following snaps to be removed: $snapname." 
+    done
+        read -p "Continue (y/n) " -n 1 -r
+        echo 
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+        snap list --all | awk '/disabled/{print $1, $3}' |
+        while read snapname revision; do
         snap remove "$snapname" --revision="$revision"
     done
-
+    fi
